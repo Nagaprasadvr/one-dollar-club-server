90d9974d91f8964445a23b29c9133088fdddd660
NagaprasadVr <nagaprasadvr246@gmail.com>
2024-06-02 21:31:57 +0530

    allow get

--- index.ts
+++ index.ts
@@ -45,7 +45,7 @@ const urls: Urls[] = [
 const CORS_HEADERS = {
   headers: {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST",
+    "Access-Control-Allow-Methods": "OPTIONS, POST,GET",
     "Access-Control-Allow-Headers": "Content-Type",
   },
 };
